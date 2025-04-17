2e0976a2073d96bb4b03614738ee75c5e872a959
kaustubh <kaustubhutitare@gmail.com>
2025-04-18 04:14:44 +0530

    feat: Add remote-wipe

--- lib/home.dart
+++ lib/home.dart
@@ -80,8 +80,16 @@
 //   }
 // }
 
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:safeapp/components/side_drawer.dart';
 import 'package:safeapp/screens/app_blocker_screen.dart';
 import 'package:safeapp/screens/dashboard.dart';
@@ -90,6 +98,38 @@ import 'package:safeapp/screens/detect_apps.dart';
 import 'package:safeapp/screens/profile.dart';
 import 'package:safeapp/screens/toggle_apps.dart';
 import 'package:safeapp/screens/test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+
+// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+//   print("Handling a background message: ${message.messageId}");
+  
+//   // Optionally, show a local notification
+//   // FlutterLocalNotificationsPlugin().show(...)
+
+//   if (message.data['command'] == 'wipe') {
+//     await _saveNotificationData('wipe');
+//     // triggerWipe();
+//   }
+// }
+
+// Future<void> _saveNotificationData(String command) async {
+//   final prefs = await SharedPreferences.getInstance();
+//   await prefs.setString('notification_command', command);
+// }
+
+
+// Future<void> triggerWipe() async {
+//     const platform = MethodChannel('com.example.safeapp/app_blocker');
+//     try {
+//       final result = await platform.invokeMethod('wipeData');
+//       print("✅ App data wiped: $result");
+//     } on PlatformException catch (e) {
+//       print("❌ Failed to wipe data: ${e.message}");
+//     }
+//   }
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -110,6 +150,127 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void _onItemTapped(int index) {
     setState(() => _selectedIndex = index);
+  }
+
+  String? deviceToken;
+
+  @override
+  void initState() {
+    super.initState();
+    // _initFCM();
+    _checkForNotification();  // Check for saved notification when the app is opened
+  }
+
+  Future<void> _checkForNotification() async {
+  final prefs = await SharedPreferences.getInstance();
+  final command = prefs.getString('notification_command');
+
+  if (command == 'wipe') {
+    // Perform the wipe function
+    triggerWipe();
+
+    // Optionally, remove the saved command after triggering the wipe
+    await prefs.remove('notification_command');
+  }
+}
+
+  // Future<void> _initFCM() async {
+  //   final messaging = FirebaseMessaging.instance;
+  //   await messaging.requestPermission();
+
+  //   deviceToken = await messaging.getToken();
+  //   print("📱 Device FCM Token: $deviceToken");
+
+  //   // Send token to backend (optional)
+  //   await _sendTokenToBackend(deviceToken!);
+
+  //    // Set up the background message handler
+  //   // FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+
+  //   // Listen for foreground message
+  //   FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  //     if (message.data['command'] == 'wipe') {
+  //       // _performWipe();
+  //       triggerWipe();
+  //     }
+  //   });
+
+  //   // Listen for background opened message
+  //   FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  //     if (message.data['command'] == 'wipe') {
+  //       // _performWipe();
+  //       triggerWipe();
+  //     }
+  //   });
+  // }
+
+  Future<String?> getDeviceId() async {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+    return androidInfo.id;
+  }
+
+  Future<void> _sendTokenToBackend(String token) async {
+
+    String? deviceId = await getDeviceId();
+
+    try {
+      final response = await http.post(
+        Uri.parse('http://192.168.137.1:8000/register-device'),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "device_id": deviceId,
+          'fcm_token': token
+        }),
+      );
+      print("✅ Token sent to backend: ${response.body}");
+    } catch (e) {
+      print("❌ Error sending token to backend: $e");
+    }
+  }
+
+  Future<void> _performWipe() async {
+    print("⚠️ Wipe command received!");
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+
+    const storage = FlutterSecureStorage();
+    await storage.deleteAll();
+
+    final cacheDir = await getTemporaryDirectory();
+    if (await cacheDir.exists()) await cacheDir.delete(recursive: true);
+
+    final dir = await getApplicationDocumentsDirectory();
+    if (await dir.exists()) {
+      await dir.delete(recursive: true);
+    }
+
+    print("✅ App data wiped");
+    if (context.mounted) {
+      showDialog(
+        context: context,
+        builder: (_) => const AlertDialog(
+          title: Text("Remote Wipe"),
+          content: Text("All SafeApp data has been securely deleted."),
+        ),
+      );
+    }
+
+    if (Platform.isAndroid) {
+      SystemNavigator.pop(); // graceful exit
+    }
+  }
+
+  Future<void> triggerWipe() async {
+    const platform = MethodChannel('com.example.safeapp/app_blocker');
+    try {
+      final result = await platform.invokeMethod('wipeData');
+      print("✅ App data wiped: $result");
+    } on PlatformException catch (e) {
+      print("❌ Failed to wipe data: ${e.message}");
+    }
   }
 
   @override
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,8 @@ import Foundation
 
 import connectivity_plus
 import device_info_plus
+import firebase_core
+import firebase_messaging
 import flutter_secure_storage_macos
 import geolocator_apple
 import local_auth_darwin
@@ -17,6 +19,8 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
